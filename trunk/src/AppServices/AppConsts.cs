f0304d32bc406610a7d28364e1ecc94e90760fa2
citikiwi <kenwu.sh@gmail.com>
2014-09-13 20:30:55 +0800

    add multiple languages

---
+++ trunk/src/AppServices/AppConsts.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ken99.AppServices
+{
+    public sealed class AppConsts
+    {
+        public sealed class UmbDic
+        {
+            public const string TopNavigation_HomeTitle = "TopNavigation_HomeTitle";
+            public const string FeaturePageWidget_Title = "FeaturePageWidgetTitle";
+
+            public const string RecentPostWidget_Title = "RecentPostWidgetTitle";
+            public const string RecentPostWidget_BrowseArchiveButton = "RecentPostWidget_BrowseArchiveButton";
+
+            public const string RecentPostOverviewWidget_ContinueReading = "RecentPostOverviewWidget_ContinueReading";
+
+            public const string SocialFollow_Title = "SocialFollow_Title";
+        }
+    }
+}
